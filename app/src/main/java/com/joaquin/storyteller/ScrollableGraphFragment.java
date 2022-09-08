b0f2b1297f445d4660d504d258b6170edd69b35b
Jimmy <joaquin.fox@gmail.com>
2022-09-08 12:55:39 -0700

    click listeners on picture thumbnails

--- app/src/main/java/com/joaquin/storyteller/ScrollableGraphFragment.java
+++ app/src/main/java/com/joaquin/storyteller/ScrollableGraphFragment.java
@@ -7,41 +7,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ScrollableGraphFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ScrollableGraphFragment extends Fragment {
+    private GridLayout mPictureGrid;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ScrollableGraphFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ScrollableGraphFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ScrollableGraphFragment newInstance(String param1, String param2) {
         ScrollableGraphFragment fragment = new ScrollableGraphFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +31,24 @@ public class ScrollableGraphFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scrollable_graph, container, false);
+        View parentView = inflater.inflate(R.layout.fragment_scrollable_graph, container, false);
+        mPictureGrid = parentView.findViewById(R.id.picture_grid);
+        for(int i =0; i <mPictureGrid.getChildCount();i++){
+            TextView pictureView = (TextView) mPictureGrid.getChildAt(i);
+            pictureView.setOnClickListener(this::onPictureClick);
+        }
+
+        return parentView;
+    }
+
+    private void onPictureClick(View view) {
+        Toast.makeText(this.getActivity(), "picture click", Toast.LENGTH_SHORT).show();
     }
 }
