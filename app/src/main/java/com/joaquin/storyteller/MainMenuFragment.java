6794abee7222ccafe3a24efa6fdf64a5e9ecc1c0
Jimmy <joaquin.fox@gmail.com>
2022-09-08 12:35:36 -0700

    basic interface with toast messaging

--- app/src/main/java/com/joaquin/storyteller/MainMenuFragment.java
+++ app/src/main/java/com/joaquin/storyteller/MainMenuFragment.java
@@ -1,5 +1,6 @@
 package com.joaquin.storyteller;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainMenuFragment extends Fragment {
 
@@ -29,6 +32,24 @@ public class MainMenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+        View parentView=inflater.inflate(R.layout.fragment_main_menu, container, false);
+        Button slideshowButton = parentView.findViewById(R.id.slideshow_button);
+        slideshowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(),"BUILDME: Call to SlideshowListView, which is a menu of slideshows", Toast.LENGTH_LONG).show();
+            }
+
+
+        });
+        Button addButton = parentView.findViewById(R.id.add_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "BUILDME: Call to AddPicturesActivity, which is an interface to add pictures. ", Toast.LENGTH_LONG).show();
+            }
+        });
+        return parentView;
     }
 }
